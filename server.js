782a2239d666f001a4360e6d0a19c5f5eae0e0e0
James <james_gower@outlook.com>
2018-04-23 11:46:59 +0100

    added projects route and projects.hbs view. Removed maintenance view

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use((req, res) => {
-	res.render('maintenance.hbs');
-});
+// app.use((req, res) => {
+// 	res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -37,6 +37,12 @@ app.get('/', (req, res) => {
 	res.render('home.hbs', {
 		pageTitle: 'Home',
 		welcomeMessage: 'Welcome!',
+	});
+});
+
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects',
 	});
 });
 
